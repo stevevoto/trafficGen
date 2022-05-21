da24a6290c8b473b925ce886050e347ac2d8f187
stevevoto <stevevoto@live.com>
2022-05-21 12:29:08 -0400

    Update TrafficGeneratorUbuntu.sh

--- TrafficGeneratorUbuntu.sh
+++ TrafficGeneratorUbuntu.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-#--------------Web Traffic Generator Ubuntu V.2 -------#
+--------------Web Traffic Generator Ubuntu -------
 
 apt-get -y install wget net-tools lshw ntp tcpdump iperf iperf3 git tcpdump htop curl
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -f install
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install git
-sudo apt-get install nodejs
+sudo apt-get -y install git
+sudo apt-get -y install nodejs
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-git clone https://github.com/stevevoto/trafficGen/.git
-cd web-traffic-generator/
+git clone https://github.com/stevevoto/trafficGen.git
+
+cd trafficGen/
+chmod +x install.sh 
 sudo ./install.sh
-git pull
-sudo reboot now
+sudo chmod 755 usr/bin/traffgen 
 wget www.office.com
 wget www.microsoft.com/en-us/microsoft-teams/log-in
 wget dialin.teams.microsoft.com
@@ -22,3 +23,5 @@ node traffgen.js
 sudo systemctl start web-traffic-gen.service
 sudo systemctl status web-traffic-gen.service
 sudo systemctl enable web-traffic-gen.service
+
+sudo reboot now
